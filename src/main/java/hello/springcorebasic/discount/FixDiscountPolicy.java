feafdc379bd96ae0e15760d99fce30cffb575325
seyoung5744 <seyoung574458@gmail.com>
2023-04-06 22:24:08 +0900

    feat : 주문과 할인 도메인 개발
    - 회원은 상품을 주문할 수 있다.
    - 회원 등급에 따라 할인 정책을 적용할 수 있다.
    - 할인 정책은 모든 VIP는 1000원을 할인해주는 고정 금액 할인을 적용해달라. (나중에 변경 될 수
    있다.)
    - 할인 정책은 변경 가능성이 높다. 회사의 기본 할인 정책을 아직 정하지 못했고, 오픈 직전까지 고민을
    미루고 싶다. 최악의 경우 할인을 적용하지 않을 수 도 있다. (미확정)

---
+++ src/main/java/hello/springcorebasic/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.springcorebasic.discount;
+
+import hello.springcorebasic.member.Grade;
+import hello.springcorebasic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000; // 1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        // Enum 타입은 == 로 비교
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
