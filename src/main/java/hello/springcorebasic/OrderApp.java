f3e75c7fe06e2454f41f1f6771bca796b1079e3e
seyoung5744 <seyoung574458@gmail.com>
2023-04-07 22:27:36 +0900

    test : 주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/springcorebasic/OrderApp.java
@@ -0,0 +1,28 @@
+package hello.springcorebasic;
+
+import hello.springcorebasic.member.Grade;
+import hello.springcorebasic.member.Member;
+import hello.springcorebasic.member.MemberService;
+import hello.springcorebasic.member.MemberServiceImpl;
+import hello.springcorebasic.order.Order;
+import hello.springcorebasic.order.OrderService;
+import hello.springcorebasic.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args){
+        MemberService memberService = new MemberServiceImpl();
+        OrderService  orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+
+    }
+
+}
---
+++ src/test/java/hello/springcorebasic/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package hello.springcorebasic.order;
+
+import hello.springcorebasic.member.Grade;
+import hello.springcorebasic.member.Member;
+import hello.springcorebasic.member.MemberService;
+import hello.springcorebasic.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        //given
+        Long memberId = 1L; // 객체 생성 단계 시 Null 여부를 고려하여 Long을 사용.
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
+
