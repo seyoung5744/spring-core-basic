22c4e7fd9d2e6ef29595ce785c1366f6143a56ca
seyoung5744 <seyoung574458@gmail.com>
2023-04-10 22:26:48 +0900

    feat : 스프링으로 전환하기

--- src/main/java/hello/springcorebasic/AppConfig.java
+++ src/main/java/hello/springcorebasic/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.springcorebasic;
 
 import hello.springcorebasic.discount.DiscountPolicy;
-import hello.springcorebasic.discount.FixDiscountPolicy;
 import hello.springcorebasic.discount.RateDiscountPolicy;
 import hello.springcorebasic.member.MemberRepository;
 import hello.springcorebasic.member.MemberService;
@@ -9,17 +8,23 @@ import hello.springcorebasic.member.MemberServiceImpl;
 import hello.springcorebasic.member.MemoryMemberRepository;
 import hello.springcorebasic.order.OrderService;
 import hello.springcorebasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
             memberRepository(),
@@ -27,6 +32,7 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/springcorebasic/MemberApp.java
+++ src/main/java/hello/springcorebasic/MemberApp.java
@@ -3,13 +3,19 @@ package hello.springcorebasic;
 import hello.springcorebasic.member.Grade;
 import hello.springcorebasic.member.Member;
 import hello.springcorebasic.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        // AppConfig에 있는 환경 설정 정보를 갖고 스프링이 Bean을 등록해서 관리해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
