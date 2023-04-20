4faf36bcec9c8c5aa6663d6792f295ca95f2016c
seyoung5744 <seyoung574458@gmail.com>
2023-04-20 22:50:35 +0900

    refactor: 테스트를 위한 코드 복구

--- src/main/java/hello/springcorebasic/order/OrderServiceImpl.java
+++ src/main/java/hello/springcorebasic/order/OrderServiceImpl.java
@@ -3,15 +3,20 @@ package hello.springcorebasic.order;
 import hello.springcorebasic.discount.DiscountPolicy;
 import hello.springcorebasic.member.Member;
 import hello.springcorebasic.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
