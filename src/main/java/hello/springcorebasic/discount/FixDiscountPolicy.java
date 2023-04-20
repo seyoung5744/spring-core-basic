176f8ebf7855ccc3cbe4a4ba7c0d264caeeae747
seyoung5744 <seyoung574458@gmail.com>
2023-04-20 22:56:28 +0900

    test : 조회 빈이 2개 이상일 때 NoUniqueBeanDefinitionException 문제 발생

--- src/main/java/hello/springcorebasic/discount/FixDiscountPolicy.java
+++ src/main/java/hello/springcorebasic/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.springcorebasic.discount;
 
 import hello.springcorebasic.member.Grade;
 import hello.springcorebasic.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
