7a25fbefbc0800e7d688b86f9fce24f91069deb0
seyoung5744 <seyoung574458@gmail.com>
2023-04-23 22:04:07 +0900

    test : 빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/springcorebasic/lifecycle/NetworkClient.java
+++ src/test/java/hello/springcorebasic/lifecycle/NetworkClient.java
@@ -1,13 +1,10 @@
 package hello.springcorebasic.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
-    public NetworkClient(){
+    public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
     }
 
@@ -20,27 +17,24 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("connect: " + url);
     }
 
-    public void call(String message){
+    public void call(String message) {
         System.out.println("call: " + url + " message = " + message);
     }
 
     // 서비스 종료시 호출
-    public void disconnect(){
+    public void disconnect() {
         System.out.println("close: " + url);
     }
 
 
-    // 의존 관계 주입이 끝나면 호출해 주겠다.
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
