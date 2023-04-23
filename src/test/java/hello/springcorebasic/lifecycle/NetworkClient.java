4280a86c1d6fc04bf2b140482c841583dba0181d
seyoung5744 <seyoung574458@gmail.com>
2023-04-23 22:22:36 +0900

    test : 애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/springcorebasic/lifecycle/NetworkClient.java
+++ src/test/java/hello/springcorebasic/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.springcorebasic.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -26,13 +29,14 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
-
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
